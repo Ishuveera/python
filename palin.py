5b7f21b33cb0b9a20f9d55937f5f0a1b067b23a4
Ishuveera <50647154+Ishuveera@users.noreply.github.com>
2019-07-05 16:14:17 +0530

    Create palin.py

---
+++ palin.py
@@ -0,0 +1,7 @@
+num = input('enter any number')
+try:
+  val = int(num)
+  if num == str(num[::-1}:
+    print('the given number is palindrome')
+   else:
+    print('the given no is not a palindrome')
